ffc484903e8f393e35bf718c9a5e8eb7fda1e326
Yashd07 <yashd1272@gmail.com>
2024-11-24 12:38:43 +0530

    db connection

--- rbac-be/config/db.js
+++ rbac-be/config/db.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+import { connect } from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -11,4 +11,4 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-module.exports = connectDB;
+export default connectDB;
